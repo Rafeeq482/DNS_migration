f56a6e8fdeff84335616d00dc5661aa9801be938
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-23 17:47:46 +0530

    this is the new commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Reference the EXISTING hosted zone in the destination account
 data "aws_route53_zone" "existing_zone" {
   name         = var.domain_name
   private_zone = false

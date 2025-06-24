e4c12e25b8eee5a80d2cd0ec7b82bdfb1290f690
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-24 12:11:36 +0530

    This is the new commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "domain_name" {
-  description = "The domain name for the hosted zone"
-  type        = string
-}
-
-variable "zone_id" {
-  description = "Route53 Hosted Zone ID"
-  type        = string
-}
-
-# No need for data lookup anymore
+# No variable declarations here

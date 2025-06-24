badcc91590f6afaa72da7021c364a8a7770c787c
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-24 13:29:21 +0530

    This is the new commit

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "domain_name" {
-  description = "The domain name for the hosted zone"
-  type        = string
-}
-
-variable "zone_id" {
-  description = "Route53 Hosted Zone ID"
-  type        = string
-}
